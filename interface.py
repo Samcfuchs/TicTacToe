52f9c5f2e914890910584301f3ed760115a98857
Sam Fuchs <samcfuchs@gmail.com>
2015-05-17 22:31:03 -0400

    Changed name of class interface to 'game' so it would make more sense on instantiation. Added class Player off of which player programs must be inherited. This may resolve the issue of passing data between Game and Player. Added some more functions to Game, all compiling successfully. Player class is just a placeholder until I add real content.

--- interface.py
+++ interface.py
@@ -1,70 +1,93 @@
-class Interface:
+class Player(object):
+    """docstring for Player"""
+    def __init__(self, arg):
+        #super(Player, self).__init__()
+        self.arg = arg
+
+
+class Game:
     """Deals with updating board and game arbitration"""
     # Note that player1 will always go first and will
     # always be X. Player2 will be O.
     def __init__(self, player1, player2):
-        #super(Interface, self).__init__()
-        if player1 = 'player':
+        #super(Game, self).__init__()
+        if player1 == 'player':
+            pass
             # pull from pvp
-        elif player1 = 'roteAI':
+        elif player1 == 'roteAI':
+            pass
             # pull from roteAI
-        elif player1 = 'learnAI':
+        elif player1 == 'learnAI':
+            pass
             # pull from learnAI
 
-        if player2 = 'player':
+        if player2 == 'player':
+            pass
             # pull from pvp
-        elif player2 = 'roteAI':
+        elif player2 == 'roteAI':
+            pass
             # pull from roteAI
-        elif player2 = 'learnAI':
+        elif player2 == 'learnAI':
+            pass
             # pull from learnAI
 
         self.result = False
         self.turn = 0
         self.board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
 
-    def is_over:
-        if (board[0] == 'X' and board[1] == 'X' and board[2] == 'X') or\
-           (board[0] == 'O' and board[1] == 'O' and board[2] == 'O'):
+    def is_over():
+        if (board[0][0] == 'X' and board[0][1] == 'X' and board[0][2] == 'X') or\
+           (board[0][0] == 'O' and board[0][1] == 'O' and board[0][2] == 'O'):
             return True
         #mid horiz
-        elif (board[3] == 'X' and board[4] == 'X' and board[5] == 'X') or\
-             (board[3] == 'O' and board[4] == 'O' and board[5] == 'O'):
+        elif (board[1][0] == 'X' and board[1][1] == 'X' and board[1][2] == 'X') or\
+             (board[1][0] == 'O' and board[1][1] == 'O' and board[1][2] == 'O'):
             return True
         #low horiz
-        elif (board[6] == 'X' and board[7] == 'X' and board[8] == 'X') or\
-             (board[6] == 'O' and board[7] == 'O' and board[8] == 'O'):
+        elif (board[2][0] == 'X' and board[2][1] == 'X' and board[2][2] == 'X') or\
+             (board[2][0] == 'O' and board[2][1] == 'O' and board[2][2] == 'O'):
             return True
 
         #left vert
-        elif (board[0] == 'X' and board[3] == 'X' and board[6] == 'X') or\
-             (board[0] == 'O' and board[3] == 'O' and board[6] == 'O'):
+        elif (board[0][0] == 'X' and board[1][0] == 'X' and board[2][0] == 'X') or\
+             (board[0][0] == 'O' and board[1][0] == 'O' and board[2][0] == 'O'):
             return True
         #mid vert
-        elif (board[1] == 'X' and board[4] == 'X' and board[7] == 'X') or\
-             (board[1] == 'O' and board[4] == 'O' and board[7] == 'O'):
+        elif (board[0][1] == 'X' and board[1][1] == 'X' and board[2][1] == 'X') or\
+             (board[0][1] == 'O' and board[1][1] == 'O' and board[2][1] == 'O'):
             return True
         #right vert
-        elif (board[2] == 'X' and board[5]  == 'X'and board[8] == 'X') or\
-             (board[2] == 'O' and board[5] == 'O' and board[8] == 'O'):
+        elif (board[0][2] == 'X' and board[1][2]  == 'X'and board[2][2] == 'X') or\
+             (board[0][2] == 'O' and board[1][2] == 'O' and board[2][2] == 'O'):
             return True
 
         #left-right diag
-        elif (board[0] == 'X' and board[4] == 'X' and board[8] == 'X') or\
-             (board[0] == 'O' and board[4] == 'O' and board[8] == 'O'):
+        elif (board[0][0] == 'X' and board[1][1] == 'X' and board[2][2] == 'X') or\
+             (board[0][0] == 'O' and board[1][1] == 'O' and board[2][2] == 'O'):
             return True
         #right-left diag
-        elif (board[2] == 'X' and board[4] == 'X' and board[6] == 'X') or\
-             (board[2] == 'O' and board[4] == 'O' and board[6] == 'O'):
+        elif (board[0][2] == 'X' and board[1][1] == 'X' and board[2][0] == 'X') or\
+             (board[0][2] == 'O' and board[1][1] == 'O' and board[2][0] == 'O'):
             return True
         else:
             return False
 
-        def turn:
-            if self.turn % 0 == 0:
-                # call on player1
-            else:
-                # call on player2
-            self.turn += 1
+    def turn():
+        if self.turn % 0 == 0:
+            pass
+            # call on player1
+        else:
+            pass
+            # call on player2
+        self.turn += 1
 
-        def return_board:
-            # compiles board into a string that is passed to an end.
+    def printboard():
+        print '''
+         %s | %s | %s
+        -----------
+         %s | %s | %s
+        -----------
+         %s | %s | %s
+        ''' % (board[0][0][0],board[0][0][1],board[0][0][2],\
+               board[0][1][0],board[0][1][1],board[0][1][2],\
+               board[0][2][0],board[0][2][1],board[0][2][2])
