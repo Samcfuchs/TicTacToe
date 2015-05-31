c5005982b3a006235ee377d7f116f186ac2e7a16
Sam Fuchs <samcfuchs@gmail.com>
2015-05-31 19:32:40 -0400

    moved game method out of init

--- interface.py
+++ interface.py
@@ -29,18 +29,6 @@ class Game:
         self.turn = 0
         self.board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']
 
-        # Begin playing game
-        # consider moving this to another function. We are seeing errors where
-        # the game calls the human, which then asks for the board (from the
-        # game) and the we get an error because the game is not yet initialized.
-        # TODO
-        turn = 0
-        while self.is_over() == False:
-            if turn % 2 == 0:
-                player1.turn()
-            elif turn % 2 == 1:
-                player2.turn()
-            turn += 1
     def is_over(self):
         if (self.board[0][0] == 'X' and self.board[0][1] == 'X' and self.board[0][2] == 'X') or\
            (self.board[0][0] == 'O' and self.board[0][1] == 'O' and self.board[0][2] == 'O'):
@@ -88,3 +76,13 @@ class Game:
         ''' % (self.board[0][0],self.board[0][1],self.board[0][2],\
                self.board[1][0],self.board[1][1],self.board[1][2],\
                self.board[2][0],self.board[2][1],self.board[2][2])
+
+    def play(self):
+        # TODO
+        turn = 0
+        while self.is_over() == False:
+            if turn % 2 == 0:
+                player1.turn()
+            elif turn % 2 == 1:
+                player2.turn()
+            turn += 1
